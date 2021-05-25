50b89b383bb7c387f085637449191f80e35050ae
Lana <kassembayeva@gmail.com>
2021-05-25 02:16:54 +0000

    array head function

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  if(array.length>1) {let answer = array[0];
+  return answer;
+} else {
+  return 'undefined'
+}
+};
+
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
