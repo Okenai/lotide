a105d7c15980dbe058045d596381f738793221b3
Lana <kassembayeva@gmail.com>
2021-05-26 04:35:06 +0000

    Updated files

--- tail.js
+++ tail.js
@@ -1,4 +1,5 @@
-const assertEqual = function(actual, expected) {
+
+/*const assertEqual = function(actual, expected) {
   if (actual.toString() === expected.toString()) {
     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
   } else {
@@ -12,7 +13,25 @@ const tail = function(array) {
   return answer;
 };
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]),["Lighthouse", "Labs"]);
+assertEqual(["Lighthouse", "Labs"],["Lighthouse", "Labs"]);
 
-assertEqual(tail([5, 7, 8]),[1, 2]);
+//assertEqual(tail([5, 7, 8]),[1, 2]);*/
 
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const tail = function(array) {
+  let answer = array.slice(1);
+  return answer;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); 
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
