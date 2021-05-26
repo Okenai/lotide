c4aaa8cd1f71752e548ad057d493adabfda6114b
Lana <kassembayeva@gmail.com>
2021-05-26 22:40:07 +0000

    function comparing arrays

---
+++ eqArrays.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+    return false;
+    }
+  }
+  return true;
+};
