23a9713d8709bac2cc6417b7fd6bfd59a404f91b
Lana <kassembayeva@gmail.com>
2021-05-27 04:41:44 +0000

    function that find the key by a value

---
+++ findKeyByValue.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+    return key;
+    }
+  }
+  return undefined;
+};
+
+
