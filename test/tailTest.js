9bc4998a0a3f11e20262e150812823178aae7f28
Lana <kassembayeva@gmail.com>
2021-05-31 22:28:29 +0000

    a test file for tail function was refactored for Mocha framework and chai assertion library

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,9 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+describe("#tail", () => {
+  it("should return 'Labs' from ['Hello', 'Lighthouse', 'Labs']" , () =>{
+  assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Labs"])
+});
+});
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); 
