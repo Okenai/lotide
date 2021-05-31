20bcb2416ccc1b1452146870639b3da81d074880
Lana <kassembayeva@gmail.com>
2021-05-31 20:56:47 +0000

    a function was refactored to modular

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-    return false;
-    }
-  }
- }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (firstArray, secondArray) {
   if (firstArray.length !== secondArray.length) {
@@ -23,3 +12,4 @@ const assertArraysEqual = function (firstArray, secondArray) {
 }
 };
 
+module.exports = assertArraysEqual;
